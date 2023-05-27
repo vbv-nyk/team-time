227865b22c8fef67753884d8020362a5443bdc12
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-05-27 15:09:12 +0530

    Add component to display team details

---
+++ app/Components/Team-Details/TeamDetails.tsx
@@ -0,0 +1,17 @@
+interface TeamDetails {
+  name: String;
+  description: String;
+  requirements: String[];
+}
+export default function TeamDetails(props: TeamDetails) {
+  const requirementTags = props.requirements.map((req, i) => (
+    <div key={i}>{req}</div>
+  ));
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <div className="flex flex-row gap-2">{requirementTags}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import CreateTeam from "./Components/Forms/CreateTeam";
 import Navbar from "./Components/Navbar/Navbar";
 import TeamCard from "./Components/Team-Card/TeamCard";
+import TeamDetails from "./Components/Team-Details/TeamDetails";
 import TeamsContainer from "./Components/Teams-Container/TeamsContainer";
 
 export default function Home() {
   return (
     <>
-      <CreateTeam />
+      <TeamDetails
+        name={"Team 1"}
+        description={"This is the first team ever created on the platform"}
+        requirements={[
+          "UI/UX Designer",
+          "Web Developer",
+          "Machine learning engineer",
+        ]}
+      />
     </>
   );
 }
