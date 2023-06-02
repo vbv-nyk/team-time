dd19d1d24f3cd50d1083bc79b274a1508ca2bd50
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-06-02 08:25:44 +0530

    Fetching data from server using apollo client

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
-import CreateTeam from "./teams/create/page";
+"use client";
 import Navbar from "./Components/Navbar/Navbar";
-import TeamDetails from "./Components/TeamDetails/TeamDetails";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 export default function Home() {
+  const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache: new InMemoryCache(),
+  });
+  // const client = ...
+
+  client
+    .query({
+      query: gql`
+        query Teams {
+          teams {
+            name
+            desc
+            reqs
+            id
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+
   return (
     <>
       <Navbar />
