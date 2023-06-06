6662a3ac1bd070bf0b0c90cb4309af69328cecbe
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-06-06 14:19:07 +0530

    removed tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Navbar from "./Components/Navbar/Navbar";
 import { useQuery, gql } from "@apollo/client";
-import { getTeams } from "./services/quries";
+import { getTeams } from "./services/queries";
 
 export default function Home() {
   return (
